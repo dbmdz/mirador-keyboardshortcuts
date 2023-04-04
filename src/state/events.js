b05599cba93d295f50e6c15dd4ae740368fcb816
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2023-04-04 09:29:03 +0200

    Add variable for the key mapping

--- src/state/events.js
+++ src/state/events.js
@@ -9,22 +9,29 @@ const KeyboardEventTypes = {
   TOGGLE_FULLSCREEN: "toggle-fullscreen",
 };
 
+/** This mapping must always have the same keys as defined in KeyboardEventTypes! */
+const keyMapping = {
+  [KeyboardEventTypes.NAVIGATE_TO_FIRST_CANVAS]: "ctrl+left",
+  [KeyboardEventTypes.NAVIGATE_TO_LAST_CANVAS]: "ctrl+right",
+  [KeyboardEventTypes.NAVIGATE_TO_NEXT_CANVAS]: "right,space",
+  [KeyboardEventTypes.NAVIGATE_TO_PREVIOUS_CANVAS]: "left",
+  [KeyboardEventTypes.TOGGLE_FULLSCREEN]: "enter",
+};
+
 const createKeyboardEventsChannel = () =>
   eventChannel((emit) => {
-    hotkeys("left", () => {
-      emit(KeyboardEventTypes.NAVIGATE_TO_PREVIOUS_CANVAS);
-    });
-    hotkeys("ctrl+left", () => {
-      emit(KeyboardEventTypes.NAVIGATE_TO_FIRST_CANVAS);
-    });
-    hotkeys("right,space", () => {
-      emit(KeyboardEventTypes.NAVIGATE_TO_NEXT_CANVAS);
-    });
-    hotkeys("ctrl+right", () => {
-      emit(KeyboardEventTypes.NAVIGATE_TO_LAST_CANVAS);
-    });
-    hotkeys("enter", () => {
-      emit(KeyboardEventTypes.TOGGLE_FULLSCREEN);
+    Object.values(KeyboardEventTypes).forEach((eventType) => {
+      const key = keyMapping[eventType];
+      if (!key) {
+        // eslint-disable-next-line no-console
+        console.warn(
+          `No key for event type ${eventType} was found in the mapping.`
+        );
+        return;
+      }
+      hotkeys(key, () => {
+        emit(eventType);
+      });
     });
     return () => hotkeys.unbind();
   });
