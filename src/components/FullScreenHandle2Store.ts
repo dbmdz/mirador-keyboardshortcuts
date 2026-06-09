d708ccbf98c9387fbaec926499d9806a942fb5f0
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2026-06-09 14:10:12 +0200

    Add explanation for the dummy component

--- src/components/FullScreenHandle2Store.ts
+++ src/components/FullScreenHandle2Store.ts
@@ -6,6 +6,10 @@ interface Props {
   storeFullScreenHandle: (handle: FullScreenHandle) => void;
 }
 
+/**
+ * This component is a workaround because the fullscreen state is not handled in the store.
+ * For this reason we store the handle retrieved from the context in the store to have access to it in our saga.
+ */
 const FullScreenHandle2Store = ({ storeFullScreenHandle }: Props) => {
   const handle = useContext<FullScreenHandle>(FullScreenContext);
   useEffect(() => {
